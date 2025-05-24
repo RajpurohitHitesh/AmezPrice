bf899ea1f1485762d620e1b58def98e5ddae2692
Hitesh Rajpurohit <iamrajpurohithitesh@gmail.com>
2025-05-24 16:25:48 +0530

    Update fontawesome.php

--- config/fontawesome.php
+++ config/fontawesome.php
@@ -1,7 +1,5 @@
 <?php
-if (!function_exists('fa_kit_url')) {
-    function fa_kit_url() {
-        $config = include config_path('globals.php');
-        return "https://kit.fontawesome.com/{$config['fontawesome']['kit_id']}.js";
-    }
-}
+return [
+    'kit_id' => '6a410e136c'
+];
+?>
